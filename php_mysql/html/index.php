1f58e12f1f33834d383528593f1b86739ef3f93e
Game <jeerasakananta@gmail.com>
2025-01-06 18:44:30 +0700

    Update index.php: Change navbar brand link to point to the homepage and rename menu management link for clarity.

--- php_mysql/html/index.php
+++ php_mysql/html/index.php
@@ -10,7 +10,7 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light ">
-        <a class="navbar-brand" href="#">หน้าเเรก</a>
+        <a class="navbar-brand" href="/index.php">หน้าเเรก</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -22,7 +22,7 @@
                     <a class="nav-link" href="/menu.php">ดูเมนู <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link" href="/contact.php">เพิ่มเมนู <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="/contact.php">จัดการเมนู <span class="sr-only">(current)</span></a>
                 </li>
             </ul>
         </div>
